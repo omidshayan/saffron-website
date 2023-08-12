b7d109192b149fa3328477954ada125a8becfab8
unknown <omidshayan.afg@gmail.com>
2023-08-12 23:54:33 +0430

    add all page for php

---
+++ php/footer.php
@@ -0,0 +1,60 @@
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="footer-about">
+                        <p><strong>آدرس:</strong> <br>هرات، فرقه ابتدای سرک نوآباد نبش کوچه مسجد جامع شرکت آریانا زعفران
+                            <br><br>
+                            0783326040 - 0796313017</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3">
+                    <h3>دسترسی سریع</h3>
+                    <ul>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <h3>دیگر لینک ها </h3>
+                    <ul>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                        <li><a href="">صفحه اصلی</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-social">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <ul>
+                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright">copyright: 2022 - <a href="aboutMe.html">کاظم علیزاده</a></div>
+    </footer>
+
+    <!-- scripts -->
+    <script src="js/all.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/grid.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title> سایت آریانا زعفران</title>
+</head>
+<body>
+
+    <!-- header -->
+    <section class="home-page">
+        <header>
+            <div class="container">
+                <nav class="navbar">
+                    <div class="ham-menu">
+                        <span></span>
+                    </div>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link">صفحه اصلی</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="products.php" class="nav-link">محصولات</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="recipes.php" class="nav-link">دستور پخت</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="about-us.php" class="nav-link">درباره ما</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="contact.php" class="nav-link">تماس با ما</a>
+                        </li>
+                    </ul>
+                    <div class="logo">
+                        <img src="img/logo.png" alt="">
+                    </div>
+                </nav>
+            </div>
+
+            <!-- bg for mobile menu -->
+            <div class="overlay"></div>
+
+            <!-- mobile menu -->
+            <div class="nav-res">
+                <div class="close-menu">
+                    <i class="fas fa-times"></i>
+                </div>
+                <img src="img/logo.png" alt="">
+                <ul>
+                    <li><a href="index.php">صفحه اصلی </a></li>
+                    <li><a href="products.php">محصولات</a></li>
+                    <li><a href="recipes.php">دستور پخت</a></li>
+                    <li><a href="about-us.php">درباره ها</a></li>
+                    <li><a href="contact.php">تماس با ما</a></li>
+                </ul>
+            </div>
+        </header>
+    </section>
