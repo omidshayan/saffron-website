9c294dd5795ab7733942fb54082299ec7606fd05
unknown <omidshayan.afg@gmail.com>
2023-08-19 00:11:32 +0430

    admin login

--- php/admin/back/login-check.php
+++ php/admin/back/login-check.php
@@ -1,3 +1,27 @@
 <?php
 
-echo 'login chexk';
+include_once "../../db.php"; 
+
+$username = $_POST['username']; //ali
+$pass = $_POST["password"]; //1234
+
+
+$sql = "SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?";
+$result = $connect->prepare($sql);
+$result->bindValue(1,$username);
+$result->bindValue(2,$pass);
+$result->execute();
+$row = $result->rowCount();
+
+
+    if($row == 1){
+        header('location:../admin-panel.php');
+        exit();
+    }
+
+    else
+    {
+        echo header('location:../index.php?error=10');
+    }
+
+
