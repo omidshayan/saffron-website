c8c66617dd1dec38a339b7ff068838a49ee1223b
unknown <omidshayan.afg@gmail.com>
2023-08-20 03:47:33 +0430

    create admin dashboard

--- php/admin/back/login-check.php
+++ php/admin/back/login-check.php
@@ -4,16 +4,12 @@ include_once "../../db.php";
 
 $username = $_POST['username']; //ali
 $pass = $_POST["password"]; //1234
-
-
 $sql = "SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?";
 $result = $connect->prepare($sql);
 $result->bindValue(1,$username);
 $result->bindValue(2,$pass);
 $result->execute();
 $row = $result->rowCount();
-
-
     if($row == 1){
         header('location:../admin-panel.php');
         exit();
