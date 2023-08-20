9f72e7b9565d62aeee40649f51d6d0b72605a7c4
unknown <omidshayan.afg@gmail.com>
2023-08-21 03:53:19 +0430

    deleted font and style

--- php/products.php
+++ php/products.php
@@ -1,30 +1,30 @@
 <?php include_once 'header.php' ?>
-    <!---------------- start content ---------------->
-    <div class="container">
-        <div class="top-text">
-            <span class="home-title">همه محصولات</span>
-        </div>
-        <hr class="hr-style">
-        <div class="row">
-        <?php
-            include_once "db.php";
-            $sql1 = "SELECT * FROM `products` ORDER BY `id` DESC";
-            $result1 = $connect->query($sql1);
-            $rows1 = $result1->fetchAll(PDO::FETCH_ASSOC);
-            foreach ($rows1 as $row1) { ?>
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="product-item">
-                        <a href="views/product.php?id=<?= $row1['id'] ?>">
-                            <img src="img-posts/<?= $row1['img'] ?>" alt="">
-                            <h4><?= $row1['name'] ?></h4>
-                            <p><?= substr($row1['description'], 0, 20) ?>...</p>
-                        </a>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
+<!---------------- start content ---------------->
+<div class="container">
+    <div class="top-text">
+        <span class="home-title">همه محصولات</span>
     </div>
-    <!---------------- end content ---------------->
+    <hr class="hr-style">
+    <div class="row">
+        <?php
+        include_once "db.php";
+        $sql1 = "SELECT * FROM `products` ORDER BY `id` DESC";
+        $result1 = $connect->query($sql1);
+        $rows1 = $result1->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rows1 as $row1) { ?>
+            <div class="col-12 col-sm-6 col-md-3">
+                <div class="product-item">
+                    <a href="product.php?id=<?= $row1['id'] ?>">
+                        <img src="img-posts/<?= $row1['img'] ?>" alt="">
+                        <h4><?= $row1['name'] ?></h4>
+                        <p><?= substr($row1['description'], 0, 20) ?>...</p>
+                    </a>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
+<!---------------- end content ---------------->
 
-    <!-- footer -->
-    <?php include_once 'footer.php' ?>
+<!-- footer -->
+<?php include_once 'footer.php' ?>
